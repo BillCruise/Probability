91501e5fd4bb7d189df572346d935e7bb66520e9
Bill Cruise <cruise02@gmail.com>
2018-02-25 09:17:58 -0500

    Added code for flipping n coins puzzle.

---
+++ scripts/flipping_n_coins.py
@@ -0,0 +1,29 @@
+# Flipping n coins
+# You have 100 coins and you flip them all at the same time.
+# Any that come up tails you set aside. The ones that come up
+# heads you flip again. How many rounds do you expect to play
+# before all of the coins have come up tails?
+# What if you start with 1000 coins?
+
+import random
+
+def flipping_n_coins():
+    samples = 10000
+    rounds = 0
+    
+    for i in range(samples):
+        coins = 100
+        while coins > 1:
+            heads = 0
+            for i in range(coins):
+                if random.random() <= 0.5:
+                    heads += 1
+            rounds += 1
+            coins = heads
+
+    average = rounds / samples
+    print("Average number of rounds:", average)
+
+
+if __name__ == '__main__':
+    flipping_n_coins()
